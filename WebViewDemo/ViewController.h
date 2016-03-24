aa5aaf89b78ecbc446d1ae14536deff9f9d5fdff
wadahana <wadahana@gmail.com>
2016-03-24 21:18:41 +0800

    Initial Commit

---
+++ WebViewDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WebViewDemo
+//
+//  Created by 吴昕 on 3/24/16.
+//  Copyright © 2016 ChinaNetCenter. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ WebViewDemo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  WebViewDemo
+//
+//  Created by 吴昕 on 3/24/16.
+//  Copyright © 2016 ChinaNetCenter. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
